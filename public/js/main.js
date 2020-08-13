1a29019455707f9ceb9ad17d5e96fd9f92b2b94c
Pororo-droid <yjwsis@kookmin.ac.kr>
2020-08-13 21:18:36 +0900

    debug

--- public/js/main.js
+++ public/js/main.js
@@ -124,7 +124,7 @@ $(document).ready(function() {
                 let owner_uid = res.owner_uid;
                 let conference_contents = res.conference_contents;
                 let conference_uid = res.conference_uid;
-                let conferenceChat_url = `https://localhost:8081/chat/${conference_uid}_${myNAME}`; //edit
+                let conferenceChat_url = `http://localhost:8081/chat/${conference_uid}_${myNAME}`; //edit
                 let conferenceVideo_url = `https://localhost:8080/index.html?roomName=${conference_uid}&userName=${myNAME}`; //edit
                 let conferenceEditCode = (myUID==owner_uid?`<div class="col"><button type="button" class="btn btn-link" data-toggle="modal" data-target="">회의 편집</button></div>` : '');
                 let code =`
